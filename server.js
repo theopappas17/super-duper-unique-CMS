7f5e130795ed7153513b1fc98bbcad06ba530f06
theopappas17 <theopappas17@gmail.com>
2022-06-14 22:17:45 -0400

    Preliminary connection to SQL from JS

---
+++ server.js
@@ -0,0 +1,14 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // MySQL password
+    password: "password",
+    database: "cms_db",
+  },
+  console.log(`Connected to the cms_db database.`)
+);
